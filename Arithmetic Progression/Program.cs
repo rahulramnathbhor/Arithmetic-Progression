df5f6693162b0c6fd9d315ce0ad7bdf87ca523e9
rahul <rahul@LAPTOP-T6G63HRD>
2022-10-11 07:50:11 +0530

    Add project files.

---
+++ Arithmetic Progression/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetic_Progression
+{
+   
+
+    class MainClass
+    {
+
+        public static void Main()
+
+        {
+
+            int n, a, d;
+
+            Console.Write("Enter the number of terms in the A.P.");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter first term and common difference of A.P.");
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            d = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                Console.Write("{0} ", a);
+
+                a = a + d;
+
+            }
+
+            Console.WriteLine("");
+
+        }
+
+    }
+}
